e124b7f8f483af8a2284c79c435a15c5965212ac
GJQ Holdings 4 <gjqholdings4@iMac.localdomain>
2019-03-07 16:25:59 +0000

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { TwoComponent }      from './two/two.component';
+import { OneComponent }      from './one/one.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+ { path: 'two', component: TwoComponent },
+ { path: 'one', component: OneComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TwoComponent } from './two/two.component';
+import { OneComponent } from './one/one.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TwoComponent,
+    OneComponent
   ],
   imports: [
     BrowserModule,
